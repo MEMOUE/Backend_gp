215c96bd04feb84b12d7d5f9e186df33d41b3004
MEMOUE <konememoue1998@gmail.com>
2024-10-19 12:55:49 +0000

    2em mis a jour(maj)

--- backend/settings.py
+++ backend/settings.py
@@ -12,9 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8q4t9rc=@xn=u1g4!cre=tly=sespg%d%d@kk_rhb(@_y3yaav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [' memko.pythonanywhere.com','gpmonde.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 
